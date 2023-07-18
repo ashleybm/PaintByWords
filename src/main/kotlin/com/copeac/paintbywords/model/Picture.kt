4ec0341921358aafb593abb6543e96ca846ec251
Ashley Baker <ashleybm@umich.edu>
2023-07-18 00:37:18 -0400

    Started working on the Picture creation read model and best storage procedure.

--- src/main/kotlin/com/copeac/paintbywords/model/Picture.kt
+++ src/main/kotlin/com/copeac/paintbywords/model/Picture.kt
@@ -1,5 +1,8 @@
 package com.copeac.paintbywords.model
 
+import java.awt.image.BufferedImage
+import java.io.BufferedReader
+import java.io.FileReader
 import java.sql.Date
 
 data class Picture(
@@ -12,8 +15,45 @@ data class Picture(
     val dateCompleted: Date?,
     val firstUser: String,
     val lastUser: String?,
-    val reminderBankId: String
+    val reminderBankId: String,
+    val progressedPicture: List<List<Int>>,
+    var recentGeneration: BufferedImage
 ) {
+    fun generateImage(): BufferedImage {
+        val generateImage = BufferedImage(xSize, ySize, BufferedImage.TYPE_INT_RGB)
+
+
+        recentGeneration = generateImage
+        return recentGeneration
+    }
+
+    fun convertCSVtoProgressedPicture(csvFilePath: String): List<List<Int>> {
+        val image = BufferedImage(xSize, ySize, BufferedImage.TYPE_INT_RGB)
+        val graphics = image.createGraphics()
+
+        BufferedReader(FileReader(csvFilePath)).use { reader ->
+            var y = 0
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                val values = line!!.split(",")
+                val pictureAsInts = values.map { wordCellCount ->
+                    wordCellCount.toInt()
+
+                }
+                var x = 0
+                for (value in values) {
+                    val squareValue = value.trim().toInt()
+                    val squareColor = getColorForSquare(squareValue) // Define the color based on the value
+                    graphics.color = squareColor
+                    graphics.fillRect(x, y, 1, 1) // Draw a single pixel square
+                    x++
+                }
+                y++
+            }
+            return TODO()
+        }
+    }
+
     fun truePixelSizeX(): Int {
         return xSize
     }
